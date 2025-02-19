6c50e01481142a2c9203fbc82db7483e42b88004
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-02-19 21:12:44 +0300

    Added Interfaces!BusinessLayer!

--- BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b14f96e58607e4d4f6439c1adf1bcb33daf9e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7dfbbac68d78924d8874b08ef418fc3ae03d3a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
