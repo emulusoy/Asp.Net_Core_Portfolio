5382b29c7aeacf57dbac56bec76f5ea490746951
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-02-20 13:51:46 +0300

    Partial view additions were made using ViewComponents, first part feature listing was made, data was extracted from SQL

---
+++ Asp.Net_Core_Portfolio/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_Portfolio.ViewComponents.Feature
+{
+    public class FeatureList:ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values=featureManager.GetList();
+            return View(values);
+        }
+    }
+}
--- Asp.Net_Core_Portfolio/obj/Debug/net8.0/Asp.Net_Core_Portfolio.AssemblyInfo.cs
+++ Asp.Net_Core_Portfolio/obj/Debug/net8.0/Asp.Net_Core_Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.Net_Core_Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7dfbbac68d78924d8874b08ef418fc3ae03d3a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c50e01481142a2c9203fbc82db7483e42b88004")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.Net_Core_Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.Net_Core_Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
