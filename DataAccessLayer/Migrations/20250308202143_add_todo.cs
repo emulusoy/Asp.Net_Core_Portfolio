00b2edd2b465dddb3c1600e40a2b9dceafadebfa
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-03-08 23:23:07 +0300

    .

---
+++ DataAccessLayer/Migrations/20250308202143_add_todo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_todo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoLists",
+                columns: table => new
+                {
+                    TodoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TodoContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TodoStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoLists", x => x.TodoID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoLists");
+        }
+    }
+}
---
+++ EntityLayer/Concrete/TodoList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class TodoList
+    {
+        [Key]
+        public int TodoID { get; set; }
+        public string TodoContent { get; set; }
+        public bool TodoStatus { get; set; }
+    }
+}
